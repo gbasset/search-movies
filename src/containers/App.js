6609b00a2564f54d8447eb123f54315e029dbd6e
Gaetan <gaetan@protonmail.com>
2019-12-16 07:47:21 +0100

    prepare the project for a new component theaters

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
         currentMovie: {},
         casting: [],
         castingdirection: [],
-        salles: ['zizi'],
+        salles: [],
         genres: [],
         video: [],
     }
